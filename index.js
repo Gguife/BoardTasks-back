180412e763840365dd1d0ad37f45183690d83360
Guilherme Gomes <gguife747@gmail.com>
2023-06-13 21:05:55 -0300

    criacao do middleware de autenticacao do swagger

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const swaggerOptions = {customCssUrl: '/swagger-ui.css'};
+const swaggerOptions = { customCssUrl: '/swagger-ui.css' };
+const app = express();
+require('dotenv').config();
 
-//Importacao do middleware
-const authDocProduction = require('./src/middlewares/authDoc')
+//Importcao do middleware
+const authDocProducao = require('./src/middlewares/authDoc');
 
 //Importacao de Rotas
 const routes = require('./src/routes');
 
-const app = express();
-require('dotenv').config();
-
-//Configuraçoes express
+//Configuracao express
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,20 +22,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Rotas para documentaçao do swagger
+
+//Rotas para documentacao do swagger
 if(process.env.NODE_ENV !== 'test'){
-  const swaggerFile = require('./swagger/swagger_output.json');
-  app.get('/', (req, res) => {/* #swagger.ignore = true */ res.redirect('/doc') });
-  app.use('/doc', authDocProduction, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+    const swaggerFile = require('./swagger/swagger_output.json');
+    app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
+    app.use('/doc', authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 }
+
 
 //Rotas
 routes(app);
 
-//Inicializacao do servidor 
-if(process.env.NODE_ENV !== 'test'){
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+
+//Inicializacao do servidor
+if (process.env.NODE_ENV !== 'test') {
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
 
 module.exports = app;
--- src/middlewares/authDoc.js
+++ src/middlewares/authDoc.js
@@ -1,38 +1,38 @@
-async function authDocProduction (req, res, next){
-  const { senhaDigitada } = req.body; //Corpo da requisicao
-  
-  //Quando o usuario estevier no lacalhost
-  if(req.headers.host.includes('localhost') || req.orignalUrl !== "/doc/"){
-    return next();
-  }
+async function authDocProducao(req, res, next) {
+    const { senhaDigitada } = req.body;
 
-  //Quando o usuario digitar a senha correta
-  if(senhaDigitada === process.env.SWAGGER_SENHA_DOC){
-    return next()
-  }
+    if(req.headers.host.includes("localhost") || req.originalUrl !== "/doc/"){
+        // Usuario está no localhost 
+        return next();
+    }
 
-  //Quando o usuario digitar a senha incorreta
-  if(senhaDigitada){
-    res.status(401).set('Content-Type', 'text/html')
-    res.send(Buffer.from(`
-      <form method="post">
-        <p style="color: red;">Senha Incorreta</p>
-        <label for="senhaDigitada">Senha da Documentação:</label>
-        <input type="password" name="senhaDigitada" id="senhaDigitada" />
-        <button type="submit">Entrar</button>
-      </form>
-    `))
-  }else{
-    //Usuario ainda nao digitou a senha e esta em modo producao
-    res.status(200).set('Content-Type', 'text/html')
-    res.send(Buffer.from(`
-      <form method="post">
-        <label for="senhaDigitada">Senha da Documentação:</label>
-        <input type="password" name="senhaDigitada" id="senhaDigitada" />
-        <button type="submit">Entrar</button>
-      </form>
-    `))
-  }
+    if(senhaDigitada === process.env.SWAGGER_SENHA_DOC){
+        // Usuario digitou a senha certa
+        return next();
+    }
+
+    if(senhaDigitada){
+        // Usuario digitou a senha errada
+        res.status(401).set('Content-Type', 'text/html');
+        res.send(Buffer.from(`
+            <form method="post">
+                <p style="color: red;">Senha Errada!</p>
+                <label for="senhaDigitada">Senha da documentação:</label>
+                <input type="password" name="senhaDigitada" id="senhaDigitada" />
+                <button type="submit">Entrar</button>
+            </form>
+        `))
+    } else{
+        // Usuario ainda não digitou a senha e está em modo produção
+        res.status(200).set('Content-Type', 'text/html');
+        res.send(Buffer.from(`
+            <form method="post">
+                <label for="senhaDigitada">Senha da documentação:</label>
+                <input type="password" name="senhaDigitada" id="senhaDigitada" />
+                <button type="submit">Entrar</button>
+            </form>
+        `))
+    }
 }
 
-module.exports = authDocProduction;
+module.exports = authDocProducao;
