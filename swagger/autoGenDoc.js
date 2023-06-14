65f3e1f9910a26f655c8c0933053b9ea6785cc40
Guilherme Gomes <gguife747@gmail.com>
2023-06-14 20:49:30 -0300

    Mudando dominio do servidor de producao

--- swagger/autoGenDoc.js
+++ swagger/autoGenDoc.js
@@ -26,7 +26,7 @@ let doc = {
             description: "Servidor localhost."
         },
         {
-            url: "https://dnc-board-tasks-back.vercel.app/",
+            url: "https://boardtasks-back-gguife.vercel.app/",
             description: "Servidor de produção."
         }
     ],
