09168af03f8d63c86b76e8cb5152e61310aad01f
Guilherme Gomes <gguife747@gmail.com>
2023-06-30 15:31:37 -0300

    Criacao da rota de obter tarefas de usuario

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -28,7 +28,7 @@ router.put('/editar/:id', authUser,conectarBancoDados, async function(req, res) 
     // #swagger.tags = ['Tarefa']
     let idTarefa = req.params.id;
     let {posicao, titulo, descricao, status, dataEntrega} = req.body;
-    const usuarioCriador = req.usuarioJwt.id;
+    const usuarioLogado = req.usuarioJwt.id;
     
     const checkTarefa = await EsquemaTarefa.findOne({ _id: idTarefa, usuarioCriador: usuarioLogado });
     if(!checkTarefa){
@@ -46,6 +46,23 @@ router.put('/editar/:id', authUser,conectarBancoDados, async function(req, res) 
       })
     }
 
+  }catch (error){
+    return tratarErros(res, error);
+  }
+});
+
+router.get('/obter/usuario', authUser,conectarBancoDados, async function(req, res) {
+  try{
+    // #swagger.tags = ['Tarefa']
+    // #swagger.description = "Endpoint para obter todas as tarefas do usuario logado."
+    const usuarioLogado = req.usuarioJwt.id;
+    const respostaBD = await EsquemaTarefa.find({usuarioCriador: usuarioLogado}).populate('usuarioCriador');
+    
+    res.status(200).json({
+      status: "OK",
+      statusMensagem: "Tarefas listadas na resposta com sucesso",
+      resposta: respostaBD
+    })
   }catch (error){
     return tratarErros(res, error);
   }
