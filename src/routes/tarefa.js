40cdf4e6a79e8bd275189991eb5d69ec576189c9
Guilherme Gomes <gguife747@gmail.com>
2023-06-30 15:20:21 -0300

    Edicao de tarefas

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -23,4 +23,32 @@ router.post('/criar', authUser,conectarBancoDados, async function(req, res) {
   }
 });
 
+router.put('/editar/:id', authUser,conectarBancoDados, async function(req, res) {
+  try{
+    // #swagger.tags = ['Tarefa']
+    let idTarefa = req.params.id;
+    let {posicao, titulo, descricao, status, dataEntrega} = req.body;
+    const usuarioCriador = req.usuarioJwt.id;
+    
+    const checkTarefa = await EsquemaTarefa.findOne({ _id: idTarefa, usuarioCriador: usuarioLogado });
+    if(!checkTarefa){
+      throw new Error("Tarefa não encontrada ou pertence a outro usuário");
+    }
+    
+    const tarefaAtualizada = await EsquemaTarefa.updateOne({_id: idTarefa}, {posicao, titulo, descricao, status, dataEntrega, usuarioCriador});
+    if(tarefaAtualizada?.modifiedCount > 0){
+      const dadosTarefa = await EsquemaTarefa.findOne({_id: idTarefa}).populate('usuarioCriador');
+
+      res.status(200).json({
+        status: "OK",
+        statusMensagem: "Tarefa criada com sucesso",
+        resposta: dadosTarefa
+      })
+    }
+
+  }catch (error){
+    return tratarErros(res, error);
+  }
+});
+
 module.exports = router;
